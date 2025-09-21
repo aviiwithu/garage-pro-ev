2544368f535c0b6a9a2804a7617b889268237227
kravinash6290 <kravinash6290@gmail.com>
2025-09-21 12:20:38 +0000

    Fix: remove environment files from git

--- .next/static/chunks/src_app_(app)_layout_tsx_b2812357._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_e9348234._.js",
-  "static/chunks/node_modules_96cdc222._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_(app)_operations_complaint-dashboard_page_tsx_137880d4._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_faa12751._.js",
-  "static/chunks/node_modules_8118ef54._.js"
-],
-    source: "dynamic"
-});
