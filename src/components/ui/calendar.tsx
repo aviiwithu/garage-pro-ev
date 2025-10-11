b095008d7466df22810ff9330ec246945fd8175e
Avinash Kumar <avinash@genus.local>
2025-10-11 15:20:33 +0530

    date picked fixed

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -23,38 +23,33 @@ function Calendar({
       animate
       navLayout="around"
       classNames={{
-        // months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-        // month: "space-y-4",
-        // caption: "flex justify-center pt-1 relative items-center",
-        // caption_label: "text-sm font-medium",
-        nav_button: cn(
-          buttonVariants({ variant: "outline" }),
-          "bg-transparent p-0 opacity-50 hover:opacity-100"
-        ),
-        head_row: "flex",
-        head_cell:
-          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
-        row: "flex w-full mt-2",
-        weekday: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
-        // day: cn(
-        //   buttonVariants({ variant: "ghost" }),
-        //   "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
-        // ),
-        day_range_end: "day-range-end",
-        day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-        day_today: "bg-accent text-accent-foreground",
-        day_outside:
-          "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
-        day_disabled: "text-muted-foreground opacity-50",
-        day_range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
-        day_hidden: "invisible",
-        ...classNames,
-      }}
-      components={{
-        // PreviousMonthButton: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        // NextMonthButton: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
+        month_caption: "flex justify-center py-3 items-center",
+        caption_label: "text-sm font-medium",
+       head_row: "grid grid-cols-7 gap-0",
+       head_cell:
+         "text-muted-foreground rounded-md w-9 h-9 flex items-center justify-center font-normal text-[0.8rem]",
+       row: "grid grid-cols-7 gap-0 mt-2",
+       weekday:
+         "h-8 w-8 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+       day: "h-8 w-8 p-0 font-normal hover:bg-accent hover:text-accent-foreground aria-selected:opacity-100 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background",
+       day_range_end: "day-range-end",
+       selected:
+         "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+       today: "bg-accent text-accent-foreground",
+       day_outside:
+         "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
+       day_disabled: "text-muted-foreground opacity-50",
+       day_range_middle:
+         "aria-selected:bg-accent aria-selected:text-accent-foreground",
+       day_hidden: "invisible",
+       months_dropdown:
+         "outline-none appearance-none bg-transparent text-muted-foreground pr-8 relative",
+       ...classNames,
+          }}
+          components={{
+       PreviousMonthButton: ({ ...props }) => <button {...props}> <ChevronLeft  className="h-6 w-6 rounded-sm hover:bg-accent hover:text-accent-foreground" /> </button>,
+        NextMonthButton: ({ ...props }) => <button {...props} > <ChevronRight className="h-6 w-6 rounded-sm hover:bg-accent hover:text-accent-foreground" /> </button>
       }}
       {...props}
     />
