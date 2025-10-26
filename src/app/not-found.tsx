a38a48cfe3a260d6d9dc7d855484bd3d65862bd0
Avinash Kumar <avinash@genus.local>
2025-10-26 22:30:32 +0530

    not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div>
+        <h1>404 - Page Not Found hhjjjhjh</h1>
+        <p>The page you’re looking for doesn’t exist.</p>
+
+    </div>
+     
+  );
+}
